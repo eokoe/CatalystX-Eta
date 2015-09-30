00f8b8bfb16abc981978b130bd0f3f31b1e15941
RenatoCRON <renato.cron@gmail.com>
2015-09-30 20:40:09 -0300

    new option

--- lib/CatalystX/Eta/Controller/AutoObject.pm
+++ lib/CatalystX/Eta/Controller/AutoObject.pm
@@ -12,11 +12,27 @@ sub AutoObject_around_object {
 
     my ( $c, $id ) = @_;
 
-    my $id_can_de_negative = $self->config->{id_can_de_negative} ? '-?' : '';
     my $primary_column = $self->config->{primary_key_column} || 'id';
 
-    $self->status_bad_request( $c, message => 'invalid.int' ), $c->detach
-      unless $id =~ /^$id_can_de_negative[0-9]+$/;
+    $config->{object_verify_type} = !defined $config->{object_verify_type} ? 'int' : $config->{object_verify_type};
+
+    unless ( $config->{object_verify_type} eq 'none' ) {
+
+        if ( $config->{object_verify_type} eq 'int' ) {
+            my $id_can_de_negative = $self->config->{id_can_de_negative} ? '-?' : '';
+            $self->status_bad_request( $c, message => 'invalid.int' ), $c->detach
+              unless $id =~ /^$id_can_de_negative[0-9]+$/;
+        }
+        elsif ( $config->{object_verify_type} eq 'uuid4' ) {
+            $self->status_bad_request( $c, message => 'invalid.uuid' ), $c->detach
+              unless $id =~ /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/i;
+        }
+        else {
+            $c->log->error(
+                "CatalystX::Eta::Controller::AutoObject: unknown object_verify_type " . $config->{object_verify_type} );
+        }
+
+    }
 
     $c->stash->{object} = $c->stash->{collection}->search( { "me.$primary_column" => $id } );
     $c->stash->{ $config->{object_key} } = $c->stash->{object}->next;
